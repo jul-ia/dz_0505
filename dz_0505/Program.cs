d52dec8d0bbc96c1215babd18e2b0d4cde910664
Juli-ia <julie78sd@gmail.com>
2018-05-13 13:06:17 +0300

    test

--- dz_0505/Program.cs
+++ dz_0505/Program.cs
@@ -10,6 +10,19 @@ namespace dz_0505
     {
         static void Main(string[] args)
         {
+            int count = 0;
+
+            char c;
+            do
+            {
+                c = Console.ReadKey().KeyChar;
+                if (c == ' ')
+                    count++;
+            } while (c != '.');
+
+            Console.WriteLine("\nSpaces: {0}", count);
+
+            Console.ReadLine();
         }
     }
 }
